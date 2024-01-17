e1febfe0ea87da0ba54f4229343e1903ea77d136
davidciz <david.ciz@vsb.cz>
2024-01-17 19:40:41 +0100

    ENH: added streaming of answers

--- test_streamlit_memory.py
+++ test_streamlit_memory.py
@@ -3,16 +3,14 @@ import os
 from langchain.memory import ConversationBufferMemory
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 import streamlit as st
-from langchain.chains import LLMChain, RetrievalQA, RetrievalQAWithSourcesChain
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.prompts import PromptTemplate
-from langchain.llms import OpenAI
 from dotenv import load_dotenv
 from langchain_community.chat_models import ChatOpenAI
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores.pinecone import Pinecone
 from langchain.callbacks import StdOutCallbackHandler
-from langchain.tools import BaseTool
-from langchain_core.callbacks import StreamingStdOutCallbackHandler
+from langchain_core.callbacks import BaseCallbackHandler
 
 load_dotenv()
 
@@ -21,20 +19,34 @@ PINECONE_ENVIRONMENT = os.environ["PINECONE_ENVIRONMENT"]
 INDEX_NAME = "documentation-chat"
 
 
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text="", display_method='markdown'):
+        self.container = container
+        self.text = initial_text
+        self.display_method = display_method
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        display_function = getattr(self.container, self.display_method, None)
+        if display_function is not None:
+            display_function(self.text)
+        else:
+            raise ValueError(f"Invalid display_method: {self.display_method}")
+
+
 def process_llm_response(llm_response):
-    print(llm_response)
+    """
+    :param llm_response:
+    :return:
+    """
     answer_part = llm_response['answer']
-    answer_part = answer_part + "\n\nSources:"
-    #print(llm_response['result'])
-    #print('\n\nSources:')
+    answer_part = answer_part + "\n\nSources: \n\n"
     for source in llm_response["source_documents"]:
-        #print(source.metadata['source'])
         answer_part = answer_part + f"{source.metadata['source']} \n\n"
     return answer_part
 
 
-handler = StdOutCallbackHandler()
-llm_callbacks = [StreamingStdOutCallbackHandler()]
+#handler = StdOutCallbackHandler()
 
 # Optionally, specify your own session_state key for storing messages
 msgs = StreamlitChatMessageHistory(key="special_app_key")
@@ -46,26 +58,35 @@ if len(msgs.messages) == 0:
 vectorstore = Pinecone.from_existing_index(INDEX_NAME, OpenAIEmbeddings())
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-template = """You are an AI chatbot having a conversation with a human.
-{summaries}
-{chat_history}
-Human: {question}
-AI: """
-prompt = PromptTemplate(input_variables=["summaries", "question"], template=template)
-# Add the memory to an LLMChain as usual
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2, callbacks=llm_callbacks)
-chain_type_kwargs = {"prompt": prompt}
-qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm, chain_type="stuff", retriever=retriever,
-                                       memory=memory, return_source_documents=True, chain_type_kwargs=chain_type_kwargs)
-
-
 for msg in msgs.messages:
     st.chat_message(msg.type).write(msg.content)
 
 if prompt := st.chat_input():
     st.chat_message("human").write(prompt)
-    response = qa_chain({"question": prompt}, callbacks=[handler])
-    #response = qa_chain.call(question=prompt, human_input=prompt)
-    processed_response = process_llm_response(response)
-    #answer = response['answer']
-    st.chat_message("ai").write(processed_response)
+
+    with st.chat_message("ai"):
+        chat_box = st.empty()
+        stream_handler = StreamHandler(chat_box, display_method='write')
+
+        template = """You are an AI chatbot having a conversation with a human.
+        {summaries}
+        {chat_history}
+        Human: {question}
+        AI: """
+        prompt_template = PromptTemplate(input_variables=["summaries", "question"], template=template)
+        # Add the memory to an LLMChain as usual
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2, streaming=True, callbacks=[stream_handler])
+        chain_type_kwargs = {"prompt": prompt_template}
+        qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm, chain_type="stuff", retriever=retriever,
+                                                               memory=memory, return_source_documents=True,
+                                                               chain_type_kwargs=chain_type_kwargs)
+        full_response = ""
+    # doesn't work with the callback , callbacks=[handler]
+        for response in qa_chain.stream({"question": prompt}):
+            #full_response += (response['answer'] or "")
+            #chat_box.markdown(full_response + "▌")
+        #chat_box.markdown(full_response)
+    # #response = qa_chain.call(question=prompt, human_input=prompt)
+            processed_response = process_llm_response(response)
+            chat_box.write(processed_response)
+    # #answer = response['answer']
